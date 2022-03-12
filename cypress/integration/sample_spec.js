f2ba867b31c96da27042ce8b972f015137312586
Jon Hyatt <jhyatt1046@gmail.com>
2022-03-12 01:59:39 -0500

    Added in 2nd section

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -14,14 +14,14 @@ describe('MyFirst Test', function() {
 })
 */
 
-
+/** 
 describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
         
         cy.visit('https://example.cypress.io')
     })
 })
-
+*/
 
 
 describe('My First Test', function() {
