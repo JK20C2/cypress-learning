5475ff15c7b6942779885ffa943cc86fabc323c3
Jon Hyatt <jhyatt1046@gmail.com>
2022-03-12 01:42:29 -0500

    first commit

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,67 @@
+//Cypress Learning
+/**
+describe('MyFirst Test', function() {
+    it('Does not do much', function() {
+        //Arrange - setup intial app state
+        // - visit a web page
+        // - query for an element
+        //Act - take an action
+        // - interact with that element
+        //Assert - make an assertion
+        // - make an assertion about page content
+        expect(true.to.equal(true))
+    })
+})
+*/
+
+/** 
+describe('My First Test', function() {
+    it('Visits the Kitchen Sink', function() {
+        
+        cy.visit('https://example.cypress.io')
+    })
+})
+*/
+
+/** 
+describe('My First Test', function() {
+    it('Finds an element', function() {
+        
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+    })
+})
+*/
+
+/** 
+describe('My First Test', function() {
+    it('Makes an assertion', function() {
+        
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+
+        cy.url()
+            .should('include', '/commands/actions')
+    })
+})
+*/
+
+describe('My First Test', function() {
+    it('Gets, types and asserts', function() {
+        
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+
+        cy.url()
+            .should('include', '/commands/actions')
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
+    })
+})
+
+
